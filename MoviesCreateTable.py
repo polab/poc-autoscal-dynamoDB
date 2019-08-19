97e268821b6a56dddc67b9dce943c29d7f6704d4
Peter Olabowale <peter.olabowale>
2019-08-19 15:11:45 -0400

    Updating the movie name

--- MoviesCreateTable.py
+++ MoviesCreateTable.py
@@ -1,7 +1,8 @@
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+#dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 
 table = dynamodb.create_table(
